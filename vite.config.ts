fe3f50e05eb2210a3ad651c74aea52273e080bf3
Kelsey Young <kelseyyoung4@gmail.com>
2025-11-16 21:09:09 -0800

    Update name - Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/NotTetris/',
-})
+export default defineConfig(({ command }) => {
+  const base = command === "build" ? "/NotTetris/" : "/";
+
+  return {
+    plugins: [react()],
+    base,
+  };
+});
